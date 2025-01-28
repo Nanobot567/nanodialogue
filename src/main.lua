5a167c2555ccf36a1ee9f8352f4aa3405b8557ad
nanobot567 <567.nanobot@gmail.com>
2025-01-28 08:38:19 -0600

    remove moderndos font (used for testing)

--- src/main.lua
+++ src/main.lua
@@ -3,15 +3,12 @@ import "lib/nanodialogue"
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
-local FONT = gfx.font.new("fnt/moderndos")
-local INVERTED_FONT = gfx.font.new("fnt/moderndos-inverted")
-
 local TEXT = "hello! {wavy}testing wavy text{/wavy}. {invert}here{/invert} {shake}is some more{/shake} {delay 100}text{/delay} to check wrapping.\n\nthis should be a new page of {invert}text.\n\n{/invert}i'm very glad this works!\n\nhere's another {wavy}very very very{/wavy} long text box {invert}just to show that more things are possible!{/invert} (and here's a bunch more text to stress test the thing)"
 
 local image = gfx.image.new(64, 64, gfx.kColorBlack)
 
-local portrait = nanoPortraitDialogueBox(TEXT, image, nil, FONT, INVERTED_FONT)
-local dialogue = nanoDialogueBox(TEXT, FONT, INVERTED_FONT)
+local portrait = nanoPortraitDialogueBox(TEXT, image)
+local dialogue = nanoDialogueBox(TEXT)
 
 function pd.update()
   pd.timer.updateTimers()
